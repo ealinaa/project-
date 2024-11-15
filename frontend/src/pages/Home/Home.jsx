eea6c56fa854ea1d84e15c8912d29e708cac46b6
ealinaa <alinaadhikari108@gmail.com>
2024-11-15 23:49:23 +0545

    exploremenu

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Home.css"
-import Header from '../../components/Navbar/Headers/Header'
+import Header from '../../components/Headers/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 const Home = () => {
+
+const [category , setCategory]= useState("All")
   return (
     <div>
         <Header/>
+        
+        <ExploreMenu category={category} setCategory={setCategory}/>
     </div>
   )
 }
